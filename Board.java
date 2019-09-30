cc65007208a3e8521bf899286534ec70fd154c8c
eerielocus <eerielocus@gmail.com>
2019-09-30 11:14:19 -0700

    Uploading simple implementation of Board and Sprite for Assignment 3.

---
+++ Board.java
@@ -0,0 +1,63 @@
+/**
+ * This class provides the game board in which objects of the game (Player, Aliens, Shots)
+ * are drawn on, can move on, fire and destroy each other.
+ *
+ */
+public class Board {
+	/**
+    Constructor method for Board.
+	 */
+	public Board() {
+		
+	}
+	/**
+    Initializing method to set board dimensions, color, and key listeners.
+	 */
+	public void createBoard() {
+		
+	}
+	/**
+    Initializing method to add the Player and Aliens, and begin animations.
+	 */
+	public void createGame() {
+		
+	}
+	/**
+    This method displays the sprite for the Player when alive, and hides sprite when dead.
+	 */
+	public void drawPlayer() {
+		
+	}
+	/**
+    This method displays the sprite for the Alien when alive, and hides sprite when destroyed.
+	 */
+	public void drawAlien() {
+		
+	}
+	/**
+    This method displays the sprite for the cannon shot.
+	 */
+	public void drawShot() {
+		
+	}
+	/**
+    This method controls the movement of the Player based on key presses.
+	 */
+	public void move() {
+		
+	}
+	/**
+    This method calls on the draw methods and color properties for the graphics.
+	 */
+	public void paintComponents() {
+		
+	}
+	/**
+    This method handles the actions involved in moving both player and alien,
+    movements for both and shots/bombs fired, events when shots hit alien or 
+    bomb hits player.
+	 */
+	public void animationCycle() {
+		
+	}
+}
