5e8c53ec784e8130b8dc0d179e40b170d70dd21b
Guiller Dalit <gelierdalit@gmail.com>
2019-10-26 17:49:49 -0700

    Updated Alien Class.

--- Implementation/src/edu/sjsu/cs151/spaceinvader/model/Alien.java
+++ Implementation/src/edu/sjsu/cs151/spaceinvader/model/Alien.java
@@ -1,45 +1,37 @@
 package edu.sjsu.cs151.spaceinvader.model;
 
-/**
- * Alien class extends the Sprite class and contains the necessary methods
- * and properties to provide action for the Alien object.
- *
- */
+
 public class Alien extends Sprite {
-	private String face = "";
-	/**
+
+    private Bomb bomb;
+    private int x, y;
+    private String face = " - ";
+    /**
 	 * Constructor method for Alien.
 	 */
-	public Alien(int x, int y) {
-		createAlien(x, y);
-	}
-	/**
+    public Alien(int x, int y){
+        initAlien(x, y);
+    }
+    /**
 	 * Initializing method to set position, set correct image, and create a bomb.
 	 * @param x position on x-axis
 	 * @param y position on y-axis
 	 */
-	public void createAlien(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-	/**
-	 * This method sets the movement of the Alien.
-	 */
-	public void act(int move) {
-		this.x += move;
-	}
-	/**
+    public void initAlien(int x, int y) {
+    	this.x = x;	
+    	this.y = y;
+    }
+    /**
 	 * This method creates a new bomb for the Alien.
 	 */
-	public static Bomb getBomb() {
-		return new Bomb();
-	}
-	
+    public static Bomb getBomb() {
+    	return new Bomb();
+    }
     public void setAlienFace(String face) {
     	this.face = face;
     }
-    
 	public String getAlienFace() {
 		return this.face;
 	}
+ 
 }
