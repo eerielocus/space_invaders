eb7aa6bf56308a2b1e747c65b8e290d568e2886c
Guiller Dalit <gelierdalit@gmail.com>
2019-11-30 10:52:55 -0800

    Updated Controller

--- Implementation/src/edu/sjsu/cs151/spaceinvader/controller/Controller.java
+++ Implementation/src/edu/sjsu/cs151/spaceinvader/controller/Controller.java
@@ -6,7 +6,7 @@ import edu.sjsu.cs151.spaceinvader.view.View;
 
 public class Controller implements GameInterface {
 
-	View view;
+	View view = new View();
 	/**
 	 * Model
 	 * 
@@ -14,38 +14,39 @@ public class Controller implements GameInterface {
 	 */
 	@Override
 	public void initGame() throws InterruptedException {
-		initCreation();
+		System.out.println("starting UI...");
+		view.start();
+		//initCreation();
 	}
 
 	@Override
 	public void initScreen() {
 	}
-
+	
 	@Override
 	public void start() throws InterruptedException {
 		initMovements();
 	}
-
 	@Override
 	public void exit() {
-	}
-	
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	protected void initCreation() {
 		Board.getInstance().createAliens();
 		Board.getInstance().createPlayer();
-		view = new View(Board.getInstance());
+		//view = new View(Board.getInstance());
 	}
 
 	protected void initMovements() throws InterruptedException {
 		Board.getInstance().initDummyMove();
 		// Board.getInstance().aliensMove();
 		// Board.getInstance().playerMove();
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	/**********
